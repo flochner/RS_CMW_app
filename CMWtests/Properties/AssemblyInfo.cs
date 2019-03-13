89bb389d6d35549020f8552d670504e07a66c950
Freddie Lochner <flochner@gmail.com>
2019-03-13 16:24:20 -0400

    changed App Icon

--- CMWtests/Properties/AssemblyInfo.cs
+++ CMWtests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Continental Resources, Inc.\nAuthor: Freddie Lochner")]
 [assembly: AssemblyProduct("CMWtests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
