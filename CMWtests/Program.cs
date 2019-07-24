273da7336dea9e51ab2e39f8acb50a831c963c13
Freddie Lochner <flochner@gmail.com>
2019-07-24 09:00:58 -0400

    removed visual styles for progress bars

--- CMWtests/Program.cs
+++ CMWtests/Program.cs
@@ -7,14 +7,14 @@ namespace CMWtests
     {
         public MainForm Obj { get => _obj; private set { } }
         private readonly MainForm _obj = null;
-
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             MainForm _obj = new MainForm();
             Application.Run(_obj);
