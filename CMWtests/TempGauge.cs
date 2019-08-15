9c4eafae066c8696a39322d0d43fc2350cc52107
Freddie Lochner <flochner@gmail.com>
2019-08-15 14:36:31 -0400

    added Stop.  Removed Update because Visa locking eliminates need for manual firing.  Added cancellation token.  Added WarmUp section in Start

--- CMWtests/TempGauge.cs
+++ CMWtests/TempGauge.cs
@@ -1,56 +1,124 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CMWtests
 {
-    partial class MainForm
+    public partial class TempGauge : UserControl
     {
-        private AutoResetEvent areTempGauge = null;
+        private CancellationTokenSource cts = null;
         private double cmwTempC = 0.0;
+        private VisaIO cmw;
 
-        private void TempGauge()
+        public TempGauge()
         {
-            areTempGauge = new AutoResetEvent(true);
-            
+            InitializeComponent();
+        }
+
+        public bool Start(VisaIO instr)
+        {
+            cmw = instr;
+            cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Task.Factory.StartNew(Run, token);
+
+            while (cmwTempC == 0.0)
+            {
+                if (MainForm.CancelTesting == true)
+                    return false;
+                Thread.Sleep(100);
+            }
+
+            while (cmwTempC < 40.0)
+            {
+                if (MainForm.CancelTesting == true)
+                    return false;
+
+                Thread.Sleep(1750);
+                Invoke(new MethodInvoker(() =>
+                {
+                    labelTemp.Text = "";
+                }));
+                Thread.Sleep(250);
+            }
+            return true;
+        }
+
+        private void Run()
+        {
+            Thread.CurrentThread.Name = "TempGauge";
+
             Invoke(new MethodInvoker(() =>
             {
-                tempSlider.Visible = true;
-                labelTemp.Visible = true;
-                panel1.Enabled = true;
-                panel1.Invalidate();
+                this.Visible = true;
+                this.Enabled = true;
+                this.Refresh();
             }));
 
-            while (cmw != null && areTempGauge != null)
+            while (cmw != null)
             {
-                areTempGauge.WaitOne();
-                cmwTempC = ReadTemp(cmw);
+#if !DEBUG
+                Thread.Sleep(5000);
+#else
+                Thread.Sleep(900);
+                labelTemp.ForeColor = System.Drawing.Color.Red;
+                Thread.Sleep(100);
+                labelTemp.ForeColor = System.Drawing.Color.Black;
+#endif
+                if (cts.IsCancellationRequested)
+                    return;
+
+                try
+                {
+                    cmwTempC = ReadTemp(cmw);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Broken at TempGauge read" +
+                                    Environment.NewLine + e.Source + ":" +
+                                    Environment.NewLine + e.Message + Environment.NewLine +
+                                    Environment.NewLine + e.StackTrace,
+                                    e.Source);
+                    Stop();
+                    return;
+                }
+
                 var sliderPos = Convert.ToInt16(((cmwTempC - 25) * 4D) + 12);
 
                 Invoke(new MethodInvoker(() =>
                 {
-                    tempSlider.Left = sliderPos;
-                    tempSlider.Invalidate();
+                    pictureBoxSlider.Left = sliderPos;
+                    pictureBoxSlider.Visible = true;
 
                     labelTemp.Text = string.Format("{0:F1}", cmwTempC);
                     labelTemp.Left = sliderPos - (labelTemp.Size.Width / 2);
-                    labelTemp.Invalidate();
+                    labelTemp.Visible = true;
+
+                    this.Refresh();
                 }));
             }
+        }
 
+        public void Stop()
+        {
             Invoke(new MethodInvoker(() =>
             {
-                tempSlider.Visible = false;
+                pictureBoxSlider.Visible = false;
                 labelTemp.Visible = false;
-                panel1.Enabled = false;
-                panel1.Invalidate();
+                this.Enabled = false;
+                this.Refresh();
             }));
-            Status = TestStatus.Abort;
+
+            cts.Cancel();
         }
 
         private double ReadTemp(VisaIO instr)
         {
+            cmw.Lock();
             var visaResponse = instr.QueryString("SENSe:BASE:TEMPerature:OPERating:INTernal?");
+            cmw.Unlock();
             var temp = Convert.ToDouble(visaResponse);
             return temp;
         }
