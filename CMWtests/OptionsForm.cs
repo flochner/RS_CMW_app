d1818b0550745a094ae45ee4ee39503721c3c447
Freddie Lochner <flochner@gmail.com>
2019-08-28 15:16:54 -0400

    added KB036 override

--- CMWtests/OptionsForm.cs
+++ CMWtests/OptionsForm.cs
@@ -5,24 +5,34 @@ namespace CMWtests
 {
     public partial class OptionsForm : Form
     {
-        public static int StatsCount { get; private set; } = 2;
-        public static bool TempOverride;
+        const int STATSRESET = 2;
+
+        public static int StatsCount { get; private set; } = STATSRESET;
+        public static bool TempOverride { get; set; }
+        public static bool KB036Override { get; set; } = true;
 
         public OptionsForm()
         {
             InitializeComponent();
-
+#if DEBUG
+            checkBoxKB036.Visible = true;
+#endif
             numericUpDown_EPS.Value = StatsCount;
             if (TempOverride == true)
                 checkBoxTempOverride.CheckState = CheckState.Checked;
             else
                 checkBoxTempOverride.CheckState = CheckState.Unchecked;
+            if (KB036Override == true)
+                checkBoxKB036.CheckState = CheckState.Checked;
+            else
+                checkBoxKB036.CheckState = CheckState.Unchecked;
         }
 
         private void buttonOK_Click(object sender, EventArgs e)
         {
             StatsCount = (int)numericUpDown_EPS.Value;
             TempOverride = (checkBoxTempOverride.CheckState == CheckState.Checked);
+            KB036Override = (checkBoxKB036.CheckState == CheckState.Checked);
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
@@ -31,8 +41,9 @@ namespace CMWtests
 
         private void btnReset_Click(object sender, EventArgs e)
         {
-            numericUpDown_EPS.Value = StatsCount = 2;
+            numericUpDown_EPS.Value = StatsCount = STATSRESET;
             checkBoxTempOverride.CheckState = CheckState.Unchecked;
+            KB036Override = (checkBoxKB036.CheckState == CheckState.Checked);
         }
     }
 }
